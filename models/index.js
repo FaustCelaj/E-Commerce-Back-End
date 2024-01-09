e8d76463e32f946f75e112cca8cadfacf3b5b79d
Faust Celaj <faustcelaj@macbook-pro.home>
2024-01-09 10:00:33 -0500

    added final adjustments

--- models/index.js
+++ models/index.js
@@ -7,23 +7,18 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-  // do i need this? 
-  onDelete: 'CASCADE'
+  onDelete: 'CASCADE',
 })
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'product_id',
-  // or category id?
-    // do i need this? 
-  onDelete: 'CASCADE',
+  foreignKey: 'category_id',
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
-    unique: flase
   },
 
   as: 'products_tags'
@@ -33,7 +28,6 @@ Product.belongsToMany(Tag, {
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
-    unique: flase
   },
 
   as: 'tags_products'
